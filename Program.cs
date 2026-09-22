bb37f8939ee2c6a2f9b7c38cb9bb10eb767cf64f
Tyler Wood <tylerwood1701@outlook.com>
2026-09-22 20:35:07 +0100

    Added: - Asking the customer for the total bill amount - Asking the customer what tip percentage they would like to give - Asking what number of people the bill needs splitting between

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("What is the total bill amount?");
+decimal billAmount = decimal.Parse(Console.ReadLine());
+
+Console.WriteLine("What is the tip percentage you would like to give?");
+decimal tipPercentage = decimal.Parse(Console.ReadLine());
+
+Console.WriteLine("What is the number of people to split the bill?");
+int numberOfPeople = int.Parse(Console.ReadLine());
+
